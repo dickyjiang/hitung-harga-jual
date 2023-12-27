d9d14857f882e436902d2cf67b317bae7f5b4494
Dicky Jiang <dickyjuwono@gmail.com>
2023-12-27 22:40:28 +0700

    add useRoute to blog

---
+++ pages/blogs/[id].vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>Blogs details {{ id }}</p>
+  </div>
+  <p>isi blog nya </p>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+</script>
+
+<style>
+
+</style>
