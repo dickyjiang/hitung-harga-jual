9fa70d439692aa3f8e23cc3c0707ae39d25817f6
Dicky Jiang <dickyjuwono@gmail.com>
2023-12-27 21:16:22 +0700

    2nd

--- app.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    Hitung Harga Jual
-  </div>
-</template>
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class=" pt-4 px-4 bg- ">
+          <h1 class="text-3xl md:text-5xl lg:text-7xl text-slate-700 font-extrabold text-center mx-4 mb-4 bg-green-">
+            Berapa <span class="text-greenbrand">harga</span>
+            jual Online Delivery?
+          </h1>
+          <div class="mb-12">
+            <h2 class="text-center text-lg md:text-xl lg:text-2xl text-slate-400 font-semibold mb-4">
+              Bingung menghitung harga jual makanan online?
+            </h2>
+            <p class="text-sm md:text-lg lg:text-xl text-center text-slate-500 md:max-w-3xl mx-auto">
+              Menghitung harga jual di platform online delivery seringkali membingungkan dengan perbedaan sistem komisi dari tiap platform ( GoFood, GrabFood, ShopeeFood) Gunakan kalkulator penghitung harga jual dibawah secara gratis.
+            </p>
+            <div class="relative z-10 text-center mt-12">
+              <a href="#" v-scroll-to="'#hitungElement'"
+                class=" bg-greenbrand  hover:bg-green-700 text-sm text-white drop-shadow-xl rounded-full font-semibold py-4 px-12 mt-8 focus:outline-none focus:shadow-outline"
+              >
+                Hitung Harga Jual </a>
+            </div>
+          </div>
+        </div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
